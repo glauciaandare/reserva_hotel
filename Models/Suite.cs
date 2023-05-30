c8f07af3d831df0be4f374d42202ffff490b2335
Glaucia Cecília Pivoto Andare <glauciaandare@gmail.com>
2023-05-30 00:53:50 -0300

    Criando classes Pessoa

---
+++ Models/Suite.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reserva_hotel.Models
+{
+    public class Suite
+    {
+        
+    }
+}
